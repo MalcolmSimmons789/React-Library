b87e3f320ef14377811b851f6e08accc3204c688
MalcolmSimmons789 <malcolm.simmons789@yahoo.com>
2022-07-10 23:16:54 -0400

    added app style file

--- react-library/src/App.js
+++ react-library/src/App.js
@@ -2,7 +2,13 @@ import React from 'react';
 import './App.css';
 
 function App() {
-	return <div className='App'></div>;
+	return (
+		<div className='App'>
+			<h1>Hello World</h1>
+			<br />
+			<h1>What a world</h1>
+		</div>
+	);
 }
 
 export default App;
